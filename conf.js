ba695a9a91fa3dbed663a4dba45cd8371d3a7dfa
Joe Nudell <joenudell@gmail.com>
2017-05-30 10:43:28 -0700

    remove unneeded deps

--- conf.js
+++ conf.js
@@ -1,4 +1,3 @@
-import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
 
@@ -6,7 +5,6 @@ export default scriptName => ({
     entry: 'munchkinland/src/' + scriptName + '.js',
     format: 'iife',
     plugins: [
-        resolve(),
         babel({ exclude: 'node_modules/**/*' })
     ],
     dest: 'munchkinland/dist/' + scriptName + '.js'
